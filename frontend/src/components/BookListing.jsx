7e97f70ef3f8f7a56cb190332e498a7fc55260c5
Sam-P96 <samnap7@gmail.com>
2026-09-22 13:59:14 +0300

    [iter3] modifies book page

--- frontend/src/components/BookListing.jsx
+++ frontend/src/components/BookListing.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BookListing = ({ book }) => {
   return (
     <div className="book-preview">
@@ -7,6 +9,9 @@ const BookListing = ({ book }) => {
       <p>Publisher: {book.publisher}</p>
       <p>Genre: {book.genre}</p>
       <p>Available: {book.availability.isAvailable ? "Yes" : "No"}</p>
+      <Link to={`/books/${book.id}`}>
+        <h2>{book.title}</h2>
+      </Link>
     </div>
   );
 };
